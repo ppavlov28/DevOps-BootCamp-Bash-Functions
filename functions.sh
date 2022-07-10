16480ba61bddeaa20c6a9655d09a46b3d06b725d
ppavlov28 <ppavlov28@gmail.com>
2022-07-11 00:41:49 +0300

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
+function pow {
+    result=$(( $1 ** $2 ))
+    echo $result 
+}
+
+function shortest {
+    params=("$@")
+    str_length=${#params[0]}
+
+    for (( n = 0; n < $#; n++)) ; do
+        if (( ${#params[$n]} == $str_length ))
+            then
+                short_param+=(${params[$n]})
+        elif
+            (( ${#params[$n]} < $str_length ))
+            then
+                str_length=${#params[$n]}
+                short_param=()
+                short_param+=(${params[$n]})
+        fi
+    done
+
+    for item in ${short_param[@]}
+    do
+        echo $item
+    done
+}
+
+function print_log {
+    echo "[$(date +"%Y-%m-%d %H:%M")] $1"
+}
+
 # Put your code here
